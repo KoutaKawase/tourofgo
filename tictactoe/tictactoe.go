40d5d674679dd61ccc034bd761974aff25544b46
KoutaKawase <suhrkawase@gmail.com>
2020-07-11 23:25:07 +0900

    😭

--- tictactoe/tictactoe.go
+++ tictactoe/tictactoe.go
@@ -15,29 +15,58 @@ func clearConsole() {
 	cmd.Run()
 }
 
+//GetWinner 現在のボードと現在のプレイヤーから勝者を出す
+func GetWinner(board [][]string, current string) string {
+	//-1してるのはX1..の部分を弾くため
+	//横一列が揃ったパターンを走査
+	for y := 0; y < len(board)-1; y++ {
+		if board[y][1] == current && board[y][2] == current && board[y][3] == current {
+			return current
+		}
+	}
+
+	//縦のパターン走査
+	//len(board[0])なのは横一列の長さを知りたかったので
+	for x := 1; x < len(board[0]); x++ {
+		if board[0][x] == current && board[1][x] == current && board[2][x] == current {
+			return current
+		}
+	}
+
+	//斜めのパターン走査
+	//左上から右下への斜め走査
+	if board[0][1] == current && board[1][2] == current && board[2][3] == current || board[0][3] == current && board[1][2] == current && board[2][1] == current {
+		return current
+	}
+
+	return ""
+}
+
+//Players プレイヤーの表記達
+var Players map[string]string = map[string]string{
+	"circle": " 0",
+	"cross":  " X",
+}
+
 func main() {
 	board := [][]string{
 		[]string{"Y1", " _", " _", " _"},
 		[]string{"Y2", " _", " _", " _"},
 		[]string{"Y3", " _", " _", " _"},
-		[]string{" ", "X1", "X2", "X3"},
+		[]string{" ", " X1", "X2", "X3"},
 	}
 	inGame := true
-	players := map[string]string{
-		"circle": " 0",
-		"cross":  " X",
-	}
-	current := players["circle"]
+	current := Players["circle"]
 	reader := bufio.NewReader(os.Stdin)
 	winner := ""
 
 	clearConsole()
 
-	for inGame {
-		for i := 0; i < len(board); i++ {
-			fmt.Printf("%s\n", strings.Join(board[i], " "))
-		}
+	for i := 0; i < len(board); i++ {
+		fmt.Printf("%s\n", strings.Join(board[i], " "))
+	}
 
+	for inGame {
 		fmt.Println("input your choice. Inputs are in X and Y order.")
 		fmt.Println("Example) 2 3")
 		fmt.Print(">> ")
@@ -61,15 +90,19 @@ func main() {
 		//仮に2が入力されたらindexは1でなければならないので-1している
 		y := choice[1] - 1
 
-		if current == players["circle"] {
-			board[y][x] = players["circle"]
-			current = players["cross"]
+		if current == Players["circle"] {
+			board[y][x] = Players["circle"]
+			winner = GetWinner(board, current)
+			current = Players["cross"]
 		} else {
-			board[y][x] = players["cross"]
-			current = players["circle"]
+			board[y][x] = Players["cross"]
+			winner = GetWinner(board, current)
+			current = Players["circle"]
 		}
 
-		winner = players["cross"]
+		for i := 0; i < len(board); i++ {
+			fmt.Printf("%s\n", strings.Join(board[i], " "))
+		}
 
 		if winner != "" {
 			inGame = false
--- tictactoe/tictactoe_test.go
+++ tictactoe/tictactoe_test.go
@@ -1,3 +1,100 @@
 package main
 
 //TODO: 勝者が存在するかテストかく
+import "testing"
+
+func Test横一列が何らかのマークで埋まってたらそのマークを勝者となる(t *testing.T) {
+	current := Players["circle"]
+	board := [][]string{
+		[]string{"Y1", " _", " _", " _"},
+		[]string{"Y2", " 0", " 0", " 0"},
+		[]string{"Y3", " _", " _", " _"},
+		[]string{" ", " X1", "X2", "X3"},
+	}
+	board2 := [][]string{
+		[]string{"Y1", " _", " _", " _"},
+		[]string{"Y2", " _", " _", " _"},
+		[]string{"Y3", " 0", " 0", " 0"},
+		[]string{" ", " X1", "X2", "X3"},
+	}
+
+	cases := []struct {
+		board   [][]string
+		current string
+		want    string
+	}{
+		{board, current, current},
+		{board2, current, current},
+	}
+
+	for _, c := range cases {
+		got := GetWinner(c.board, c.current)
+		if got != c.want {
+			t.Errorf("GetWinner(%q, %q) == %q, want %q", c.board, c.current, got, c.want)
+		}
+	}
+}
+
+func Testどれか縦一列がcurrentで埋まってたら勝者としてcurrentを返す(t *testing.T) {
+	current := Players["circle"]
+	board := [][]string{
+		[]string{"Y1", " _", " 0", " _"},
+		[]string{"Y2", " _", " 0", " _"},
+		[]string{"Y3", " _", " 0", " _"},
+		[]string{" ", " X1", "X2", "X3"},
+	}
+	board2 := [][]string{
+		[]string{"Y1", " _", " _", " 0"},
+		[]string{"Y2", " _", " _", " 0"},
+		[]string{"Y3", " _", " _", " 0"},
+		[]string{" ", " X1", "X2", "X3"},
+	}
+
+	cases := []struct {
+		board   [][]string
+		current string
+		want    string
+	}{
+		{board, current, current},
+		{board2, current, current},
+	}
+
+	for _, c := range cases {
+		got := GetWinner(c.board, c.current)
+		if got != c.want {
+			t.Errorf("GetWinner(%q, %q) == %q, want %q", c.board, c.current, got, c.want)
+		}
+	}
+}
+
+func Test斜めに揃っていたらwinnerとしてcurrentを返す(t *testing.T) {
+	current := Players["circle"]
+	board := [][]string{
+		[]string{"Y1", " 0", " _", " _"},
+		[]string{"Y2", " _", " 0", " _"},
+		[]string{"Y3", " _", " _", " 0"},
+		[]string{" ", " X1", "X2", "X3"},
+	}
+	board2 := [][]string{
+		[]string{"Y1", " _", " _", " 0"},
+		[]string{"Y2", " _", " 0", " _"},
+		[]string{"Y3", " 0", " _", " _"},
+		[]string{" ", " X1", "X2", "X3"},
+	}
+
+	cases := []struct {
+		board   [][]string
+		current string
+		want    string
+	}{
+		{board, current, current},
+		{board2, current, current},
+	}
+
+	for _, c := range cases {
+		got := GetWinner(c.board, c.current)
+		if got != c.want {
+			t.Errorf("GetWinner(%q, %q) == %q, want %q", c.board, c.current, got, c.want)
+		}
+	}
+}
